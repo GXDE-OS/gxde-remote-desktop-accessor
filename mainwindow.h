8e30a68b0a38ae845ebeee728611a4de0d7f02ae
gfdgd xi <3025613752@qq.com>
2022-06-19 08:55:18 +0800

    先立flag，这个分支是为了更好的迁移到其它Linux发行版和Windows版本

--- mainwindow.h
+++ mainwindow.h
@@ -1,29 +1,29 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 #include <QWidget>
-#include <DMainWindow>
-#include <DGuiApplicationHelper>//用来适配深色模式 Used to adapt to dark model
+#include <QMainWindow>
+//#include <QGuiApplicationHelper>//用来适配深色模式 Used to adapt to dark model
 #include "widget.h"
-#include <DPushButton>
-#include <DLabel>
+#include <QPushButton>
+#include <QLabel>
 #include <QString>
-#include <DLineEdit>
-#include <DCheckBox>
-#include <DFrame>
-#include <DRadioButton>
+#include <QLineEdit>
+#include <QCheckBox>
+#include <QFrame>
+#include <QRadioButton>
 #include <QButtonGroup>
-#include <DPasswordEdit>
-#include <DSlider>
+//#include <QPasswordEdit>
+#include <QSlider>
 #include <unistd.h>
 
-DWIDGET_USE_NAMESPACE
+//DWIDGET_USE_NAMESPACE
 
-class MainWindow : public DMainWindow
+class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(DMainWindow *parent = nullptr);
+    MainWindow(QMainWindow *parent = nullptr);
     ~MainWindow();
 private:
     QHBoxLayout *moreSettingLayout = new QHBoxLayout;
@@ -34,22 +34,22 @@ private:
     QButtonGroup *rdpVersion = new QButtonGroup(this);
     QButtonGroup *rdesktopVersion = new QButtonGroup(this);
     //QButtonGroup *rdpConnect = new QButtonGroup(this);
-    DCheckBox *paste = new DCheckBox();
-    DCheckBox *remoteSound = new DCheckBox();
-    DCheckBox *shareRootFile = new DCheckBox();
-    DCheckBox *shareHomeFile = new DCheckBox();
-    DCheckBox *moreSetting = new DCheckBox;
-    DFrame *moreSettingFrame = new DFrame;
-    DLineEdit *sizeScreenWidth = new DLineEdit();
-    DLineEdit *sizeScreenHeight = new DLineEdit();
-    DLineEdit *showTitle = new DLineEdit();
-    DLineEdit *user = new DLineEdit();
-    DLineEdit *command = new DLineEdit();
-    DPasswordEdit *password = new DPasswordEdit();
-    DSlider *color = new DSlider();
-    DLabel *colorShow = new DLabel();
+    QCheckBox *paste = new DCheckBox();
+    QCheckBox *remoteSound = new DCheckBox();
+    QCheckBox *shareRootFile = new DCheckBox();
+    QCheckBox *shareHomeFile = new DCheckBox();
+    QCheckBox *moreSetting = new DCheckBox;
+    QFrame *moreSettingFrame = new DFrame;
+    QLineEdit *sizeScreenWidth = new DLineEdit();
+    QLineEdit *sizeScreenHeight = new DLineEdit();
+    QLineEdit *showTitle = new DLineEdit();
+    QLineEdit *user = new DLineEdit();
+    QLineEdit *command = new DLineEdit();
+    //QPasswordEdit *password = new DPasswordEdit();
+    QSlider *color = new DSlider();
+    QLabel *colorShow = new DLabel();
     QWidget *w=new QWidget; //w是窗口的用户区，应当是所有窗口中控件的父（不包含标题栏及其上边的控件） W is the user area of the window and should be the parent of all controls in the window (excluding the title bar and controls above it)
-    void setTheme(DGuiApplicationHelper::ColorType);
+    //void setTheme(DGuiApplicationHelper::ColorType);
     void ConnectIp();
     void ShowMoreSetting();
     void ShowScreenEnabled();
